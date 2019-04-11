d0300c73543eeece7268886647475b7570c67188
Jun <eileenyoo2@gmail.com>
2019-04-11 10:06:50 +0900

    메일인증하고 테스트

--- app/src/main/java/com/example/git_push_test/MainActivity.java
+++ app/src/main/java/com/example/git_push_test/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //수정
     //수정2
-    //테스트 하는중
+    //메일 인증 완료. 테스트 하는중
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
